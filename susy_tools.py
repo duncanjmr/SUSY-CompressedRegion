15f1ce62cef726abf42dbc0863131a5663e93632
Duncan <duncanjmr@gmail.com>
2022-02-08 14:42:33 -0600

    Updated scan files 	new file:   run_m1_slepton_scan.py 	modified:   susy_tools.py

--- susy_tools.py
+++ susy_tools.py
@@ -9,14 +9,18 @@ import warnings
 from scipy.interpolate import interp2d
 import scipy.interpolate as interp
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from pathlib import Path
+
 
 import subprocess as sp
 from subprocess import DEVNULL, STDOUT, check_call
 
 
-scripts_dir = "/home/duncan/UChicago/SUSY/scripts"
+scripts_dir = "/home/duncan/UChicago/SUSY/compressed"
 checkmate_dir = "/home/duncan/Software/checkmate2"
 susyhit_dir = "/home/duncan/UChicago/SUSY/new_susyhit"
+micromegas_dir = "/home/duncan/UChicago/SUSY/micromegas"
 
 def changeParamValue(label, value):
     """
@@ -67,11 +71,8 @@ def getParamValue(s, label):
 
         if line[0] == "#": line[0] = " "
 
-        parind = len(line) - len(line.lstrip())
-        parlen = line[parind:].find(" ")
         
-        ind_val = len(line) - len(line[parind+parlen:].lstrip())
-        return float(line[ind_val:])
+        return float(line.split()[-1])
     
 def getBranchingRatio(s, label):
     """
@@ -190,6 +191,27 @@ def optimize_higgs(h_target=125., tolerance=0.5):
 
     return x[-1], h[-1]
 
+def get_neutralino_eigenvalues(m1, m2, tanB, mu, ordered=True):
+
+    sw = np.sqrt(0.222)
+    cw = np.sqrt(1-sw**2)
+
+    beta = np.arctan(tanB)
+    MZ = 92.
+
+    Massmatrix = np.array([[m1*cw**2 + m2 * sw**2, (m2-m1) *sw*cw, 0, 0],
+                           [(m2-m1)*sw*cw, m1*sw**2 + m2*cw**2, MZ, 0],
+                           [0, MZ, mu*np.sin(2*beta), -mu*np.cos(2*beta)],
+                           [0,  0, -mu*np.cos(2*beta), -mu*np.sin(2*beta)]])
+
+
+    masses = np.linalg.eig(Massmatrix)[0]
+    
+    if not ordered:
+        return masses
+    
+    order = np.argsort(np.abs(masses))
+    return masses[order]
 
 def get_neutralinoMassDiff(M1, M2):
     """
@@ -208,27 +230,178 @@ def get_neutralinoMassDiff(M1, M2):
         x1 = getParamValue(s, "~chi_10")
         
     os.chdir(scripts_dir)
-    return x2 - x1
+    return np.abs(x2) - np.abs(x1)
 
+def get_neutralinoMasses(M1, M2):
+    """
+    For given value of M1, M2, returns delta(x10, x20).
+    """
+    changeParamValue("M_1", M1)
+    changeParamValue("M_2", M2)
 
-def get_closestmass(M1, return_diff=False, fast=False):
+    os.chdir(susyhit_dir)
+    os.system("./run >> " + scripts_dir + "/temp.out")
+    
+    # Get particle masses from susyhit output
+    with open("susyhit_slha.out", "r") as f:
+        s = f.read()
+        x2 = getParamValue(s, "~chi_20")
+        x1 = getParamValue(s, "~chi_10")
+        
+    os.chdir(scripts_dir)
+    return np.array([np.abs(x1), np.abs(x2)])
+        
+
+def minimize_neutralinoMassDiff(M1_0, M2_0, to_minimize="M1", step = 0.5, return_diff=False, verbose=False):
     """
     Uses a linear approximation to find the M2 value which minimizes delta(x10, x20)
     """
-    dx = M1 * 0.2
-    est_lo = M1 + dx - get_neutralinoMassDiff(M1, M1 + dx)
-    est_hi = M1 + get_neutralinoMassDiff(M1, M1 - dx) - dx
+
+    if to_minimize == "M1":
+        ind = 0
+        to_min = np.array([1,0])
+    elif to_minimize == "M2":
+        ind = 1
+        to_min = np.array([0,1])
+    else:
+        raise Exception("to_minimize must be either \"M1\" or \"M2\"")
+
+    step = 0.5
+    M = [np.array([M1_0, M2_0])]
+    diffs = [get_neutralinoMassDiff(*M[0])]
+
+    dM = 0.1 * to_min
+    M.append(M[0] + dM)
+    diffs.append( get_neutralinoMassDiff(*M[-1]) )
+
+    deriv = [10]
+
+    dx = M[-1][ind] - M[-2][ind]
+    deriv.append((diffs[-1] - diffs[-2])/dx)
     
-    est = 0.5 * (est_lo + est_hi)
+    c = 0
     
+    while np.abs(deriv[-1]) > 0.05 and np.abs(deriv[-2]) > 0.05  and diffs[-1] > 0.1 and c < 5:
+
+        dM = -deriv[-1]*step*diffs[-1] * to_min
+        M.append(M[-1] + dM)
+        diffs.append(get_neutralinoMassDiff(*M[-1]))
+        dx = M[-1][ind] - M[-2][ind]
+        deriv.append((diffs[-1] - diffs[-2]) / dx)
+        
+        if verbose:
+            print(M[-1], diffs[-1])
+        
+        c += 1
+    
+    est = M[-1]
+    
+    
+    if verbose:
+        print(M[-1], diffs[-1])
+    
+    if np.abs(deriv[-1]) < 0.8:
+        
+        M.append(M[-1] + dM)
+        diffs.append(get_neutralinoMassDiff(*M[-1]))
+        l3 = np.array(M).T[ind][-3:]
+
+        #print(l3)
+        #print(diffs[-3:])
+        p, _ = curve_fit(lambda x, a, b, c: a * (x - b)**2 + c, l3, diffs[-3:], [0.05, l3[-1], diffs[-1]])
+        #print(p)
+        
+        est[ind] = p[1]
+    
+    elif deriv[-1] > 0.8 and diffs[-1] < 1:
+        est[ind] = est[ind] - diffs[-1] / deriv[-1]
+
+        
     if return_diff:
-        return est, get_neutralinoMassDiff(M1, est)
+        return est, get_neutralinoMassDiff(*est)
     else:
         return est
 
+def get_approxGm2(M1, M2, mu, m_sl, tanB):
+    alpha = 1./137
+    sw_sq = 0.25
+
+    m_mu = 0.1
+    
+    fxp = lambda x: (x**2 - 4*x + 3 + 2*np.log(x)) / (1-x)**3
+    fx0 = lambda x: (x**2 - 1 - 2*x*np.log(x)) / (1-x)**3
+    df_dx = lambda x : x/(1-x)**3 * (2*x - 2*(1 + np.log(x)) + 3 * (1-x)**2 * fx0(x))
+
+    
+    a_x0 = -alpha * M1 * m_mu**2 * mu * tanB / (4*np.pi*(1-sw_sq)*m_sl**4) * (fx0((M1/m_sl)**2) + df_dx((M1/m_sl)**2))
+    a_xp = alpha*m_mu**2 * mu * M2 * tanB / (4*np.pi*sw_sq*m_sl**2) * (fxp((M2/m_sl)**2) - fxp((mu/m_sl)**2)) / (M2**2 - mu**2)
+    
+    return a_x0 + a_xp
+    
+def optimize_sleptonMassesGm2(M1, M2, mu, tanB, max_m_sl=2005., N=200, verbose=False):
+
+    changeParamValue("mu(EWSB)", mu)
+    changeParamValue("tanbeta(MZ)", tanB)
+        
+    def get_gm2(m_sleptons):
+        
+        changeParamValue("M_eL",   m_sleptons)
+        changeParamValue("M_eR",   m_sleptons)
+        changeParamValue("M_muL",  m_sleptons)
+        changeParamValue("M_muR",  m_sleptons)
+        changeParamValue("M_tauL", m_sleptons)
+        changeParamValue("M_tauR", m_sleptons)
+        
+        queue = mp.Queue()
+        run_once(M1, M2, True, queue, working_directory="test")
+        return queue.get()["gm2"]
+    
+    xtest = np.logspace(np.log10(np.abs(M2)+10), np.log10(max_m_sl), N)
+    gm2_est = get_approxGm2(M1, M2, mu, xtest, tanB)
+
+    def get_best(xtest, gm2_est):
+        
+        #best = np.argmin(np.abs(gm2_est*1e9-2.74))
+        score = (gm2_est*1e9-2.74)
+
+        candidates = []
+        for i in range(0, len(xtest)-1):
+            if np.sign(score[i]) != np.sign(score[i+1]):
+                candidates.append(np.interp(0, [score[i+1], score[i]], [xtest[i+1], xtest[i]]))
+
+        if len(candidates) > 0:
+            return np.max(candidates), 2.74e-9
+        
+        else:
+            cand = np.argmin(np.abs(score))
+            if np.abs(score[cand]) < 0.73:
+                return xtest[cand], gm2_est[cand]
+            else:
+                return np.nan, np.nan
+                
+    
+    
+    initial, gm2_init = get_best(xtest, gm2_est)
+    
+    if np.isnan(initial): 
+        print("Warning: Unable to satisfy g-2 with these parameters: (M1, M2, mu, tanB) = (%i, %i, %i, %i)" % (M1, M2, mu, tanB))
+        
+        ind = np.argmin(np.abs(gm2_est*1e9-2.74))
+        print("\t Closest point: (m_sleptons, delta-g) = (%i, %.2E)" % (xtest[ind], gm2_est[ind]))
+        return np.nan
+    
+    gm2_susyhit = get_gm2(initial)
+    correction_size = gm2_susyhit / gm2_init
+    
+    #print(gm2_susyhit, correction_size)
+    
+    return get_best(xtest, correction_size * gm2_est)[0]
+    
+
 
 def run_once(M1, M2, remake, out_queue, run_prospino=False, 
-        run_micromegas=False, run_checkmate=False, working_directory=".", additional_command=None, index=None):
+             run_micromegas=True, run_checkmate=False, working_directory=".", 
+             additional_command=None, index=None, masses_to_save=["~chi_10", "~chi_20"]):
     """
     Runs susyhit, micromegas, checkmate, and/or prospino once. Each program can be toggled.
     """
@@ -241,6 +414,7 @@ def run_once(M1, M2, remake, out_queue, run_prospino=False,
     if isinstance(remake, bool):
         remake = {"prospino": remake, "susyhit": remake, "checkmate": remake}
 
+    # Set param values to run over
     changeParamValue("M_1", M1)
     changeParamValue("M_2", M2)
     if additional_command != None:
@@ -252,13 +426,19 @@ def run_once(M1, M2, remake, out_queue, run_prospino=False,
         os.system("./run >> " + scripts_dir + "/temp.out")
         os.chdir(cwd)
         os.system("cp " + susyhit_dir + "/susyhit_slha.out " + working_directory + "/spectra_slha/" + filename)
-
+        
     # Get particle masses from susyhit output
     with open(working_directory + "/spectra_slha/" + filename, "r") as f:
         s = f.read()
-        m_x1 = getParamValue(s, "~chi_10")
-        m_x2 = getParamValue(s, "~chi_20")
-        h = getParamValue(s, "h")
+        if np.isnan(getParamValue(s, "EWSB")):
+            out = {"index": index, "M1": M1, "M2": M2, "cx": np.nan, "r": np.nan, "analysis": np.nan }
+            for var in ["gm2", "omega_dm", "dd_pval"] + masses_to_save:
+                out[var] = np.nan
+                
+            out_queue.put(out)
+            return None
+              
+    
         
     # Generate Prospino cross sections
     if run_prospino:
@@ -283,64 +463,72 @@ def run_once(M1, M2, remake, out_queue, run_prospino=False,
             cx = float(l[l.rfind(" "):])    
     else:
         cx = np.nan
-            
-    
+                
     if run_checkmate:
         outname = "checkmate_%i_%i.dat" % (M1, M2)
         
         if outname not in os.listdir(working_directory + "/checkmate") or remake["checkmate"]:
             os.system("cp " + working_directory + 
                       "/spectra_slha/" + filename + 
-                      " " + checkmate_dir + "/bin/susyhit_slha.out")
+                      " " + checkmate_dir + "/bin/")
             
             os.chdir(checkmate_dir + "/bin")
-            os.system("python3 converter.py")
+            os.system("python runCheckmate.py " + filename)
             
-            orig = os.environ["PYTHONPATH"]
-            os.environ["PYTHONPATH"] = "/home/duncan/Software/python2/lib/python2.7/site-packages:/home/duncan/Software/root6/lib"
-            
-            maxev = 10000
-
-            sp.run(['./CheckMATE', '--name', outname, '--analysis', 'Atlas13tev', 
-                    '--slha-file', './susyhit_MGcard.dat', '-maxev', str(maxev),
-                    '-mgcommand', 'import model MSSM_SLHA2; generate p p > n2 x1+; generate p p > n2 x1-', 
-                    '-sp', '-oe', 'overwrite'], stdout=DEVNULL, stderr=STDOUT)
-
-            os.environ["PYTHONPATH"] = orig
+            os.system("rm " + filename)
             os.chdir(cwd)
            
-            os.system("cp " + checkmate_dir + "/results/" + outname + "/result.txt " + working_directory + "/checkmate/" + outname)
+            os.system("cp " + checkmate_dir + "/results/" + filename.split(".")[0] + "/result.txt " + working_directory + "/checkmate/" + outname)
 
-            
             #os.system("rm -rf ../results/" + outname)
             
-
-            
+        # Read checkmate output and get r value
         with open(working_directory + "/checkmate/" + outname) as f:
             s=f.readlines()
             r = np.nan
             for l in s:
-                line = l.split(":")
+                line = l.split(": ")
                 if line[0] == "Result for r":
                     r = float(line[1])
+                if line[0] == "Analysis":
+                    analysis = line[1]
+            
+                
               
     else:    
         r = np.nan
-
+        analysis = ""
+        
+    outdir = {"index": index, "M1": M1, "M2": M2, "cx": cx, "r": r, "analysis": analysis}
+    
     
     # Calculate g-2 contribution using micromegas
-    s = os.popen('../micromegas/MSSM/get_gm2 ./' + working_directory + '/spectra_slha/' + filename).read()
-    try:
-        gm2 = float(s.split("\n")[0])
-        om = float(s.split("\n")[1])
-    except:
-        gm2 = np.nan
-        om = np.nan
+    if run_micromegas:
+        s = os.popen(micromegas_dir + '/MSSM/get_gm2 ./' + working_directory + '/spectra_slha/' + filename).read()
 
-    out_queue.put({"M1": M1, "M2": M2, "m_x1": m_x1, "m_x2": m_x2, "cx": cx, "gm2": gm2, "omega": om, "r": r})
+        lines = s.split("\n")[:-1]
+
+        for i, var in enumerate(["gm2", "omega_dm", "dd_pval"]):
+            try:
+                outdir[var] = float(lines[i].split(": ")[1])
+            except:
+                print("Couldn't get %s from: %s" % (var, lines[i]))
+                outdir[var] = np.nan
+
+        with open(working_directory + "/micromegas_out/micromegas_%i_%i.dat" % (M1, M2), "w") as f:
+            f.write(s)
+        
+    # Get particle masses from susyhit output
+    with open(working_directory + "/spectra_slha/" + filename, "r") as f:
+        s = f.read()
+        for label in masses_to_save:
+            outdir[label] = getParamValue(s, label)
+            
+    out_queue.put(outdir)
+    return None
     
 
-def run(points_list, remake, run_prospino=False, run_micromegas=False, 
+def run(points_list, remake, run_prospino=False, run_micromegas=True, 
         run_checkmate=False,
         working_directory=None, verbose=True, additional_command=None, n_procs=3):
     
@@ -356,17 +544,11 @@ def run(points_list, remake, run_prospino=False, run_micromegas=False,
         working_directory = "output"
     
     # Set up directories
-    if working_directory not in os.listdir("."):
-        os.mkdir(working_directory)
-
-    if "spectra_slha" not in os.listdir(working_directory):
-        os.mkdir(working_directory + "/spectra_slha")    
-
-    if "prospino_cx" not in os.listdir(working_directory):
-        os.mkdir(working_directory + "/prospino_cx")   
+    Path(working_directory).mkdir(parents=True, exist_ok=True)
         
-    if "checkmate" not in os.listdir(working_directory):
-        os.mkdir(working_directory + "/checkmate")   
+    for subdir in ["spectra_slha", "prospino_cx", "checkmate", "micromegas_out"]:
+        if subdir not in os.listdir(working_directory):
+            os.mkdir(working_directory + "/" + subdir)    
     
     processors = queue.Queue()
 
@@ -414,8 +596,22 @@ def run(points_list, remake, run_prospino=False, run_micromegas=False,
                          str(datetime.timedelta(seconds=int(remaining)))), end="")
         c += 1
         
+    data = {}
+    ret = out.get()
+    for k in ret.keys():
+        data[k] = [ret[k]]
         
-    return out
+    while out.qsize() > 0:
+        ret = out.get()
+        for k in ret.keys():
+            data[k].append(ret[k])
+            
+    
+    #indsort = data.pop("index")
+    #for k in data.keys():
+    #    data[k] = [data[k][i] for i in indsort]
+        
+    return data
         
 def draw_contour_old(run_points, uls, lumi=139., unit_conversion=1., cmap=None, 
                  alpha=1, show_ul_points=False, show_pointlabels=False):
